bc7ebff15a6a3c4ecdd237acac480797341c2538
rajgawera <raj@gawera.com>
2026-02-07 18:27:32 +0000

    Added a coin toss strategy for comparison

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ import yfinance as yf
 import mplfinance as mpf
 from trading_loop import trading_loop
 from trading_types import PeriodData
-from strategies import BuyAndHold, TradingStrategy
+from strategies import BuyAndHold, TradingStrategy, CoinToss
 from typing import List
+from matplotlib.ticker import MultipleLocator
 
 # Function to get historic data and return in a pandas dataframe
-def get_trading_data(period='1y'):
+def get_trading_data(period='2y'):
     data=yf.Ticker('^GSPC').history(period=period)
-    data['Balance'] = 0
     return data
 
 # Plot the data in trades as a candlestick diagram
 def plot_data(trades, all_strategies: List[TradingStrategy]):
     extra_plots=[]
     for strategy in all_strategies:
-        extra_plots.append(mpf.make_addplot(trades[str(strategy)], panel=1, ylabel=f"{str(strategy)}", secondary_y=False))
-    mpf.plot(trades, type='candle', tight_layout=True, style='yahoo', addplot=extra_plots)
+        extra_plots.append(mpf.make_addplot(trades[str(strategy)], panel=1, label=str(strategy), secondary_y=False))
+    fig, axlist = mpf.plot(trades, type='candle', tight_layout=True, style='yahoo', addplot=extra_plots, ylabel="Price", returnfig=True)
+    axlist[0].xaxis.set_minor_locator(MultipleLocator(1))
+    axlist[0].xaxis.set_major_locator(MultipleLocator(7))
+    mpf.show()
 
 if __name__ == "__main__":
     print("Trading is fun!")
@@ -25,9 +28,11 @@ if __name__ == "__main__":
     trades = get_trading_data()
 
     trading_data = []
-    trading_strategy = BuyAndHold(money=10000)
+    initial=10000
+    trading_strategy = BuyAndHold(money=initial)
+    rand_strat = CoinToss(money=initial)
 
-    all_strategies : List[TradingStrategy] = [trading_strategy]
+    all_strategies : List[TradingStrategy] = [trading_strategy, rand_strat]
 
     for trade in trades.iterrows():
         trading_data.append(PeriodData(
@@ -37,9 +42,10 @@ if __name__ == "__main__":
             close=trade[1]["Close"],
             date=trade[0]
         ))
-    
-    balance = trading_loop(trading_data, trading_strategy)
+    for strat in all_strategies:
+        balance = trading_loop(trading_data, strat)
 
-    trades[str(trading_strategy)] = balance
+        trades[str(strat)] = balance
+        print(str(strat), "$",int(balance[-1]), int(balance[-1]/initial*100),"%")
 
     plot_data(trades, all_strategies)
--- strategies.py
+++ strategies.py
@@ -1,5 +1,6 @@
 from trading_types import PeriodData
 from typing import List
+import random
 
 class TradingStrategy:
     def __init__(self, money: float):
@@ -35,4 +36,29 @@ class BuyAndHold(TradingStrategy):
             self.shares = self.money / data[0].close
             self.money = 0
             return 
+        return
+
+class CoinToss(TradingStrategy):
+    def __init__(self, money: float):
+        super().__init__(money)
+
+    def strategy(self, data: List[PeriodData]):
+        """
+        Execute a coin toss. If heads, then buy (if not already bought). If tails, sell (if not already sold)
+
+        :param trading_history: history of trading data
+        :return: Tuple containing operation and the amount of money we want to use with the operation.
+        """
+        if len(data)==0: return
+        if random.randint(0,1)==0:
+            #buy
+            if self.money>0:
+                self.shares = self.money / data[-1].close
+                self.money = 0
+        else:
+            #sell
+            if self.shares>0:
+                self.money += self.shares * data[-1].close
+                self.shares = 0
+
         return
