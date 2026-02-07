3d3401cd2d846778c059eb7caa9f783eec092111
rajgawera <raj@gawera.com>
2026-02-07 14:39:39 +0000

    Now plots the balance on separate sub chart

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import pandas_ta_classic as ta
 from trading_loop import trading_loop
 import mplfinance as mpf
+import numpy as np
 
 # Function to get historic data and return in a pandas dataframe
 def get_trading_data(period='1y'):
@@ -9,13 +10,16 @@ def get_trading_data(period='1y'):
     return data
 
 # Plot the data in trades as a candlestick diagram
-def plot_data(trades):
-    mpf.plot(trades, volume=True, type='candle', mav=20, tight_layout=True, style='yahoo')
+def plot_data(trades, balances):
+    extra_plot = mpf.make_addplot(trades['balance'], color='#606060', panel=2, ylabel='balances', secondary_y=False)
+    mpf.plot(trades, volume=True, type='candle', tight_layout=True, style='yahoo', addplot=extra_plot)
+
 
 # take the historic trade data and wisely invest the initial_investment
 # returns a list (or amended dataframe) that contains the balance for each day during the trading period
 def execute_strategies(trades, initial_investment):
-    pass
+    trades['balance'] = np.random.default_rng().integers(1, 1000, len(trades))
+    return trades
 
 if __name__ == "__main__":
     print("Trading is fun!")
