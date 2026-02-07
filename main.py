c1f2786715a3f93496fe55fed211801897c104a1
rajgawera <raj@gawera.com>
2026-02-07 15:16:29 +0000

    Balances added to trades dataframe

--- main.py
+++ main.py
@@ -7,18 +7,19 @@ import numpy as np
 # Function to get historic data and return in a pandas dataframe
 def get_trading_data(period='1y'):
     data=yf.Ticker('^GSPC').history(period=period)
+    data['Balance'] = 0
     return data
 
 # Plot the data in trades as a candlestick diagram
 def plot_data(trades, balances):
-    extra_plot = mpf.make_addplot(trades['balance'], color='#606060', panel=2, ylabel='balances', secondary_y=False)
+    extra_plot = mpf.make_addplot(trades['Balance'], color='#606060', panel=2, ylabel='balances', secondary_y=False)
     mpf.plot(trades, volume=True, type='candle', tight_layout=True, style='yahoo', addplot=extra_plot)
 
 
 # take the historic trade data and wisely invest the initial_investment
 # returns a list (or amended dataframe) that contains the balance for each day during the trading period
 def execute_strategies(trades, initial_investment):
-    trades['balance'] = np.random.default_rng().integers(1, 1000, len(trades))
+
     return trades
 
 if __name__ == "__main__":
