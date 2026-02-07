946c1b9666aae2f18c4f0f01132a05a1991097e8
rajgawera <raj@gawera.com>
2026-02-07 13:10:31 +0000

    Get S&P 500 history from yahoo finance

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import yfinance as yf
+import pandas_ta_classic as ta
+
+def get_trading_data():
+    data=yf.Ticker('^GSPC').history(period='1y')
+    print(data)
+    return data
+
 if __name__ == "__main__":
-    print("Main Function")
+    print("Trading is fun!")
+    trades = get_trading_data()
